359ce32801dd7001241dd179dca7a9fb0f5af485
Clotilde Pouvreau <pclotilde44@hotmail.fr>
2022-09-21 17:04:12 +0200

    Ajout dossier middleware

--- backend/middleware/multer.config.js
+++ backend/middleware/multer.config.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 
-const MIME_TYPE = {
+const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
-        const extension = MIME-TYPES[file.mimetype];
+        const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
 });
