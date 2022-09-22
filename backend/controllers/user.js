8c1e88e606aaa928a8145785ae11f5e306dbead5
Clotilde Pouvreau <pclotilde44@hotmail.fr>
2022-09-22 14:07:07 +0200

    Modification messages erreur login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,12 +20,12 @@ exports.login = (req, res, next) => {
     User.findOne({email: req.body.email})
         .then(user => {
             if (!user) {
-                res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
+                res.status(401).json({ message: 'Utilisateur non trouvé !' })
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
+                        res.status(401).json({ message: 'Mot de passe incorrect !' })
                     } 
                     res.status(200).json({ 
                         userId: user._id,
