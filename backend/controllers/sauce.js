bfff256a469606cf5790e7ca3ff9f12572a743f5
Clotilde Pouvreau <pclotilde44@hotmail.fr>
2022-09-22 18:32:29 +0200

    Suppression de l'ancienne image quand elle a été modifiée

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -27,22 +27,19 @@ exports.createSauce = (req, res, next) => {
 
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ? {
-        ...JSON.parse(req.body.thing),
+        ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
     } : { ...req.body };
 
-    delete sauceObject.userId;
     Sauce.findOne({ _id: req.params.id })
-        .then ((sauce) => {
-            if (sauce.userId != req.auth.userId) {
-                res.status(403).json({ message: 'Requête non autorisée !' });
-            } else {
-                Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-                    .catch(error => res.status(400).json({ error }));
-            }
-        })
-        .catch(error => res.status(400).json({ error }));
+    .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            Sauce.updateOne({ _id: req.params.id} , {...sauceObject, _id: req.params.id})
+                .then(()=> res.status(200).json({ message: 'Sauce modifiée !'}))
+                .catch(()=> res.status(400).json({ error}))
+        });
+    });
 };
 
 exports.deleteSauce = (req, res, next) => {
