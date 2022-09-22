23d907869566c2e5d8515fca456fd4942a68bd73
Clotilde Pouvreau <pclotilde44@hotmail.fr>
2022-09-22 14:33:26 +0200

    Modification messages likes et unlikes

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -62,12 +62,12 @@ exports.likeSauce = (req, res, next) => {
     const like = req.body.like;
     if(like === 1) {
         Sauce.updateOne({_id: req.params.id}, { $inc: { likes: 1}, $push: { usersLiked: req.body.userId}, _id: req.params.id })
-        .then( () => res.status(200).json({ message: 'Vous aimez cette sauce' }))
+        .then( () => res.status(200).json({ message: 'Sauce likée' }))
         .catch( error => res.status(400).json({ error}))
 
     } else if(like === -1) {
         Sauce.updateOne({_id: req.params.id}, { $inc: { dislikes: 1}, $push: { usersDisliked: req.body.userId}, _id: req.params.id })
-        .then( () => res.status(200).json({ message: 'Vous n’aimez pas cette sauce' }))
+        .then( () => res.status(200).json({ message: 'Sauce unlikée' }))
         .catch( error => res.status(400).json({ error}))
 
     } else {
@@ -75,13 +75,13 @@ exports.likeSauce = (req, res, next) => {
         .then( sauce => {
             if( sauce.usersLiked.indexOf(req.body.userId)!== -1){
                  Sauce.updateOne({_id: req.params.id}, { $inc: { likes: -1},$pull: { usersLiked: req.body.userId}, _id: req.params.id })
-                .then( () => res.status(200).json({ message: 'Vous n’aimez plus cette sauce' }))
+                .then( () => res.status(200).json({ message: 'Annulation like' }))
                 .catch( error => res.status(400).json({ error}))
                 }
                 
             else if( sauce.usersDisliked.indexOf(req.body.userId)!== -1) {
                 Sauce.updateOne( {_id: req.params.id}, { $inc: { dislikes: -1 }, $pull: { usersDisliked: req.body.userId}, _id: req.params.id})
-                .then( () => res.status(200).json({ message: 'Vous ne détestez plus cette sauce' }))
+                .then( () => res.status(200).json({ message: 'Annulation unlike' }))
                 .catch( error => res.status(400).json({ error}))
                 }           
         })
