1a686817e82ead513419654e2af3eac507b82a86
Clotilde Pouvreau <pclotilde44@hotmail.fr>
2022-09-27 11:09:58 +0200

    Ajout packages helmet et dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
+const helmet = require("helmet");
+require('dotenv').config();
+
 const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
-const path = require('path');
 
 const app = express();
 
 // Connexion à la base de données MongoDB
-mongoose.connect('mongodb+srv://clotildep:SgMmCok7PKDaapbo@cluster1.vx6kwpr.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(process.env.SECRET_DB,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(express.json());
-
-// Déclaration des headers pour permettre les requêtes cross-origin
+// Déclaration des headers pour permettre les requêtes cross-origin (CORS)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use(express.json());
+app.use(helmet());
 
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
