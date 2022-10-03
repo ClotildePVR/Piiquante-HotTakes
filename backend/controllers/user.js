627577d81015d18b2d07c7a2365fd016e6df91f1
Clotilde Pouvreau <pclotilde44@hotmail.fr>
2022-10-03 15:58:16 +0200

    Correction messages erreurs

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -24,12 +24,12 @@ exports.login = (req, res, next) => {
     User.findOne({email: req.body.email})
         .then(user => {
             if (!user) {
-                res.status(401).json({ message: 'Utilisateur non trouvé !' })
+                return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte !' })
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        res.status(401).json({ message: 'Mot de passe incorrect !' })
+                        return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte !' })
                     } 
                     res.status(200).json({ 
                         userId: user._id,
--- backend/middleware/verify-password.js
+++ backend/middleware/verify-password.js
@@ -2,7 +2,7 @@ const passwordSchema = require('../models/password');
 
 module.exports= (req, res, next) => {
     if (!passwordSchema.validate(req.body.password)) {
-        res.status(400).json({ message: 'Le mot de passe doit faire entre 8 et 20 caractères et contenir au moins une majuscule, une minuscule, un chiffre, et ne doit pas contenir d\'espace' })
+        return res.status(400).json({ message: 'Le mot de passe doit faire entre 8 et 20 caractères et contenir au moins une majuscule, une minuscule, un chiffre, et ne doit pas contenir d\'espace' })
     } else {
         next();
     }
