c09f188a2c9b616441bf5205909206ab8f212cac
Clotilde Pouvreau <pclotilde44@hotmail.fr>
2022-10-04 14:43:21 +0200

    MAJ sécurité mail/mdp

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const userCtrl = require('../controllers/user');
 const verifyPassword = require('../middleware/check-password');
 const verifyEmail = require('../middleware/check-email');
 
-router.post('/signup', verifyPassword, verifyEmail, userCtrl.signup);
+router.post('/signup', verifyEmail, verifyPassword, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
